3466a5a81296f7f5c9a68d05e62b296f70671f4e
serhii-sierov <serhii.sierov@sysgears.com>
2023-10-19 14:39:38 +0300

    Config repository

--- core-api/.eslintrc.js
+++ core-api/.eslintrc.js
@@ -21,5 +21,17 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-floating-promises': 'warn',
+    '@typescript-eslint/no-misused-promises': 'warn',
+    '@typescript-eslint/no-throw-literal': 'warn',
+    '@typescript-eslint/return-await': ['warn', 'in-try-catch'],
   },
 };
